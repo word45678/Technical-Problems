3473a5e26404210c9eec61d933bfef3fd992edc1
word45678 <104034706+word45678@users.noreply.github.com>
2022-05-21 16:13:23 -0400

    Fixed a minor issue and added second function

--- Two_Sum.py
+++ Two_Sum.py
@@ -4,9 +4,19 @@ def twoSumIndices(data,target):
         complement = target - data[i]
         if(complement in complements.keys()):
             return [complements[complement],i]
+        complements[data[i]] = i
+
+def twoSumValues(data,target):
+    complements = {}
+    for i in range(len(data)):
+        complement = target - data[i]
+        if(complement in complements.keys()):
+            return [complement,data[i]]
+        complements[data[i]] = i
 
 
 numbers = [2,7,11,15]
 value = 9
 print("Input: " + str(numbers) + " and Target: " + str(value))
 print("Sum component locations: " + str(twoSumIndices(numbers,value)))
+print("Sum component values: " + str(twoSumValues(numbers,value)))
