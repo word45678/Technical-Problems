670af9f5e430983f3881764dd4fcb667ff6e1754
word45678 <104034706+word45678@users.noreply.github.com>
2023-01-15 12:37:11 -0500

    Trying out TKINTER
    GUIs are an interesting concept I'd like to get into

---
+++ tkinter_test.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+from hiding.Two_Sum import *
+
+
+window = tk.Tk()
+window.title("Technical Problems App")
+window.geometry("600x400")
+newlabel = tk.Label(text = "Viva Las Vegas!")
+newlabel.grid(column=0,row=0)
+window.mainloop()
